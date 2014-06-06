691f5624a6204387569cac6c45af31a17f97f01a
Marc Neuhaus <mneuhaus@famelo.com>
2014-06-06 10:57:15 +0200

    added an UserViewHelper

---
+++ Classes/Famelo/Common/ViewHelpers/UserViewHelper.php
@@ -0,0 +1,30 @@
+<?php
+namespace Famelo\Common\ViewHelpers;
+/*                                                                        *
+ * This script belongs to the TYPO3 Flow package "CE.Reports".            *
+ *                                                                        *
+ *                                                                        */
+
+use TYPO3\Flow\Annotations as Flow;
+
+class UserViewHelper extends \TYPO3\Fluid\Core\ViewHelper\AbstractViewHelper {
+
+	/**
+	 * @var \TYPO3\Flow\Security\Context
+	 * @Flow\Inject
+	 */
+	protected $securityContext;
+
+	/**
+	 *
+	 * @param string $currentUser
+	 * @return string
+	 */
+	public function render($as = 'currentUser') {
+		$templateVariableContainer = $this->renderingContext->getTemplateVariableContainer();
+		$templateVariableContainer->add($as, $this->securityContext->getParty());
+		$output = $this->renderChildren();
+		$templateVariableContainer->remove($as);
+		return $output;
+	}
+}
