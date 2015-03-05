a706d659d3b7f43aaf0f9e96af5a927277fe408f
Marc Neuhaus <mneuhaus@famelo.com>
2015-03-05 16:45:40 +0100

    added shortcut to \TYPO3\Flow\var_dump

--- Classes/Famelo/Common/Package.php
+++ Classes/Famelo/Common/Package.php
@@ -1,33 +1,40 @@
 <?php
-namespace Famelo\Common;
+namespace Famelo\Common {
 
-/*                                                                        *
- * This script belongs to the TYPO3 Flow package "Famelo.Common".         *
- *                                                                        *
- * It is free software; you can redistribute it and/or modify it under    *
- * the terms of the GNU Lesser General Public License, either version 3   *
- * of the License, or (at your option) any later version.                 *
- *                                                                        *
- * The TYPO3 project - inspiring people to share!                         *
- *                                                                        */
+	/*                                                                        *
+	 * This script belongs to the TYPO3 Flow package "Famelo.Common".         *
+	 *                                                                        *
+	 * It is free software; you can redistribute it and/or modify it under    *
+	 * the terms of the GNU Lesser General Public License, either version 3   *
+	 * of the License, or (at your option) any later version.                 *
+	 *                                                                        *
+	 * The TYPO3 project - inspiring people to share!                         *
+	 *                                                                        */
 
-use \TYPO3\Flow\Package\Package as BasePackage;
+	use \TYPO3\Flow\Package\Package as BasePackage;
 
-/**
- * The TYPO3.Expose package.
- *
- */
-class Package extends BasePackage {
 	/**
-	 * Invokes custom PHP code directly after the package manager has been initialized.
+	 * The TYPO3.Expose package.
 	 *
-	 * @param Core\Bootstrap $bootstrap The current bootstrap
-	 * @return void
 	 */
-	public function boot(\TYPO3\Flow\Core\Bootstrap $bootstrap) {
-		$dispatcher = $bootstrap->getSignalSlotDispatcher();
-		$dispatcher->connect('TYPO3\Flow\Object\DependencyInjection\ProxyClassBuilder', 'extendProxyClass', 'Famelo\Common\Proxy\AccessableProxyClassBuilder', 'buildAccessors');
-		$dispatcher->connect('TYPO3\Flow\Command\CoreCommandController', 'additionalProxyClassBuilder', 'Famelo\Common\Proxy\AccessableProxyClassBuilder', 'build');
+	class Package extends BasePackage {
+		/**
+		 * Invokes custom PHP code directly after the package manager has been initialized.
+		 *
+		 * @param Core\Bootstrap $bootstrap The current bootstrap
+		 * @return void
+		 */
+		public function boot(\TYPO3\Flow\Core\Bootstrap $bootstrap) {
+			$dispatcher = $bootstrap->getSignalSlotDispatcher();
+			$dispatcher->connect('TYPO3\Flow\Object\DependencyInjection\ProxyClassBuilder', 'extendProxyClass', 'Famelo\Common\Proxy\AccessableProxyClassBuilder', 'buildAccessors');
+			$dispatcher->connect('TYPO3\Flow\Command\CoreCommandController', 'additionalProxyClassBuilder', 'Famelo\Common\Proxy\AccessableProxyClassBuilder', 'build');
+		}
+	}
+}
+
+namespace {
+	function flowdump($variable, $title = NULL, $return = FALSE, $plaintext = NULL) {
+		\TYPO3\Flow\var_dump($variable, $title, $return, $plaintext);
 	}
 }
 
